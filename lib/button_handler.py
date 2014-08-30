f35457125e377d19d912509b3b7c7749fc5a15aa
Ppamo <pablo@ppamo.cl>
2014-08-30 21:33:58 +0000

    Change the A and B buttons hardware

--- lib/button_handler.py
+++ lib/button_handler.py
@@ -1,4 +1,4 @@
-import config, datetime
+import config, datetime, time
 import RPi.GPIO as GPIO
 
 class button_handler(object):
@@ -7,19 +7,19 @@ class button_handler(object):
 		self.config = config
 		self.a_pin = 18
 		self.b_pin = 23
-		self.c_pin = 24
+		self.c_pin = 22
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setup(self.a_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 		GPIO.setup(self.b_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 		GPIO.setup(self.c_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-		GPIO.add_event_detect(self.a_pin, GPIO.FALLING, callback=self.button_callback, bouncetime=300)
-		GPIO.add_event_detect(self.b_pin, GPIO.FALLING, callback=self.button_callback, bouncetime=300)
+		GPIO.add_event_detect(self.a_pin, GPIO.RISING, callback=self.button_callback, bouncetime=300)
+		GPIO.add_event_detect(self.b_pin, GPIO.RISING, callback=self.button_callback, bouncetime=300)
 		GPIO.add_event_detect(self.c_pin, GPIO.FALLING, callback=self.button_callback, bouncetime=300)
 		self.a_callbacks = []
 		self.b_callbacks = []
 		self.c_callbacks = []
 		self.callbacks_by_channel = { self.a_pin: self.a_callbacks, self.b_pin: self.b_callbacks, self.c_pin: self.c_callbacks }
-		self.event_delta = datetime.timedelta(milliseconds = 500)
+		self.event_delta = datetime.timedelta(milliseconds = 1000)
 		self.last_pressed_event_time = datetime.datetime.now()
 
 	def execute_callbacks(self, callbacks):
@@ -28,7 +28,7 @@ class button_handler(object):
 				callback()
 
 	def button_callback(self, channel):
-		print "button_callback: channel {}".format(channel)
+		print "button_callback: channel {};".format(channel)
 		now = datetime.datetime.now()
 		delta = now - self.last_pressed_event_time
 		self.last_pressed_event_time = now
